e469145709bfe6baa336f0740cbc86a5182a1e2a
Aryan Sharma <158464705+Team-CF-Bots@users.noreply.github.com>
2024-02-27 01:09:11 +0530

    Update restart.py

--- Romeo/plugins/restart.py
+++ Romeo/plugins/restart.py
@@ -7,7 +7,7 @@ from Romeo import SUDO_USER
 
 @Client.on_message(filters.command(["restart", "reload", "rs", "rl"], ".") & (filters.me | filters.user(SUDO_USER)))
 async def restart(client: Client, message: Message):
-    reply = await message.reply_text("**Restarting...**")
+    reply = await message.reply_text("**𝐉ᴀᴀɴ 𝐇ᴏ 𝐑ᴀʜᴀ 𝐑ᴇsᴛᴀʀᴛ...**")
     await message.delete()
-    await reply.edit_text("Successfully Restarted RomeoBot...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
+    await reply.edit_text("Successfully 𝐑ᴇsᴛᴀʀᴛᴇᴅ 𝐋ɪʟʏ 𝐔sᴇʀʙᴏᴛ...\n\n🐼 𝟏-𝟐 𝐌ɪɴ 𝐒ᴀʙᴀʀ 𝐊ᴀʀᴏ\n𝐏ʟᴜɢɪɴs 𝐋ᴏᴀᴅ 𝐇ᴏ 𝐑ᴀʜᴇ...</b>")
     os.system(f"kill -9 {os.getpid()} && python3 -m Romeo")
