1fe36b31a3085f7dd71d31e4524bcca50ecf6aeb
Aryan Sharma <158464705+Team-CF-Bots@users.noreply.github.com>
2024-02-27 02:17:53 +0530

    Update lr.py

--- Romeo/plugins/lr.py
+++ Romeo/plugins/lr.py
@@ -15,15 +15,15 @@ async def raid(app: Client, m: Message):
         counts = int(Romeo[0])
         username = Romeo[1]
         if not counts:
-          await m.reply_text(f"ʟᴏᴠᴇ ʀᴀɪᴅ ʟɪᴍɪᴛ ɴᴏᴛ ғᴏᴜɴᴅ ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴄᴏᴜɴᴛ!")
+          await m.reply_text(f"𝐋ᴏᴠᴇ 𝐑ᴀɪᴅ 𝐋ɪᴍɪᴛ 𝐍ᴏᴛ 𝐅ᴏᴜɴᴅ 𝐏ʟᴇᴀsᴇ 𝐆ɪᴠᴇ 𝐂ᴏᴜɴᴛ!")
           return       
         if not username:
-          await m.reply_text("ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ sᴘᴇᴄɪғʏ ᴀɴ ᴜsᴇʀ! ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴜsᴇʀ ᴏʀ ɢɪᴠᴇ ɪᴅ/ᴜsᴇʀɴᴀᴍᴇ")
+          await m.reply_text("𝐘ᴏᴜ 𝐍ᴇᴇᴅ 𝐓ᴏ 𝐒ᴘᴇᴄɪғʏ 𝐀ɴ 𝐔sᴇʀ! 𝐑ᴇᴘʟʏ 𝐓ᴏ 𝐀ɴʏ 𝐔sᴇʀ 𝐎ʀ 𝐆ɪᴠᴇ 𝐈ᴅ/𝐔sᴇʀɴᴀᴍᴇ")
           return
         try:
            user = await app.get_users(Romeo[1])
         except:
-           await m.reply_text("**𝐋ᴇʟᴇ 𝐋ᴀᴜᴅᴀ 𝐄ʀʀᴏʀ 𝐀ᴀɢʏᴀ:** ᴜsᴇʀ ɴᴏᴛ ғᴏᴜɴᴅ ᴏʀ ᴍᴀʏ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ!")
+           await m.reply_text("**𝐋ᴇʟᴇ 𝐋ᴀᴜᴅᴀ 𝐄ʀʀᴏʀ 𝐀ᴀɢʏᴀ:** 𝐔sᴇʀ 𝐍ᴏᴛ 𝐅ᴏᴜɴᴅ 𝐎ʀ 𝐌ᴀʏ 𝐁ᴇ 𝐃ᴇʟᴇᴛᴇᴅ!")
            return
       elif m.reply_to_message:
         counts = int(Romeo[0])
@@ -32,16 +32,16 @@ async def raid(app: Client, m: Message):
         except:
            user = m.reply_to_message.from_user 
       else:
-        await m.reply_text("ᴜsᴀɢᴇ: .ʟʀᴀɪᴅ ᴄᴏᴜɴᴛ ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ʀᴇᴘʟʏ")
+        await m.reply_text("𝐔sᴀɢᴇ: .𝐋ʀᴀɪᴅ 𝐂ᴏᴜɴᴛ 𝐔sᴇʀɴᴀᴍᴇ 𝐎ʀ 𝐑ᴇᴘʟʏ")
         return
       if int(m.chat.id) in GROUP:
-         await m.reply_text("**sᴏʀʀʏ !! ʏᴀʜᴀ sᴘᴀᴍ ɴʜɪ ʜᴏ sᴀᴋᴛᴀ.**")
+         await m.reply_text("**𝐒ᴏʀʀʏ !! 𝐘ᴀʜᴀ 𝐒ᴘᴀᴍ 𝐍ʜɪ 𝐇ᴏ 𝐒ᴀᴋᴛᴀ.**")
          return
       if int(user.id) in VERIFIED_USERS:
          await m.reply_text("𝐒ᴏʀʀʏ 𝐀ᴘ 𝐀ᴘɴᴇ 𝐁ᴀᴀᴘ 𝐏ᴀʀ 𝐑ᴀɪᴅ 𝐍ʜɪ 𝐊ᴀʀ 𝐒ᴀᴋᴛᴇ")
          return
       if int(user.id) in SUDO_USER:
-         await m.reply_text("sᴜᴅᴏ ʀᴀɴᴅɪ ᴘᴀʀ ʀᴀɪᴅ ɴʜɪ ʜᴏɢᴀ.")
+         await m.reply_text("𝐒ᴜᴅᴏ 𝐑ᴀɴᴅɪ 𝐏ᴀʀ 𝐑ᴀɪᴅ 𝐍ʜɪ 𝐇ᴏɢᴀ.")
          return
       mention = user.mention
       for _ in range(counts): 
@@ -57,10 +57,10 @@ async def draid(app: Client, m: Message):
         counts = int(Romeo[0])
         username = Romeo[1]
         if not counts:
-          await m.reply_text(f"ʟᴏᴠᴇ ʀᴀɪᴅ ʟɪᴍɪᴛ ɴᴏᴛ ғᴏᴜɴᴅ ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴄᴏᴜɴᴛ!")
+          await m.reply_text(f"𝐋ᴏᴠᴇ 𝐑ᴀɪᴅ 𝐋ɪᴍɪᴛ 𝐍ᴏᴛ 𝐅ᴏᴜɴᴅ 𝐏ʟᴇᴀsᴇ 𝐆ɪᴠᴇ 𝐂ᴏᴜɴᴛ!")
           return       
         if not username:
-          await m.reply_text("ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ sᴘᴇᴄɪғʏ ᴀɴ ᴜsᴇʀ ! 𝐑ᴇᴘʟʏ 𝐓ᴏ 𝐀ɴʏ 𝐔sᴇʀ 𝐎ʀ 𝐆ɪᴠᴇ 𝐈ᴅ/𝐔sᴇʀɴᴀᴍᴇ")
+          await m.reply_text("𝐘ᴏᴜ 𝐍ᴇᴇᴅ 𝐓ᴏ 𝐒ᴘᴇᴄɪғʏ 𝐀ɴ 𝐔sᴇʀ ! 𝐑ᴇᴘʟʏ 𝐓ᴏ 𝐀ɴʏ 𝐔sᴇʀ 𝐎ʀ 𝐆ɪᴠᴇ 𝐈ᴅ/𝐔sᴇʀɴᴀᴍᴇ")
           return
         try:
            user = await app.get_users(Romeo[1])
@@ -74,16 +74,16 @@ async def draid(app: Client, m: Message):
         except:
            user = m.reply_to_message.from_user 
       else:
-        await m.reply_text("ᴜsᴀɢᴇ: .ᴅᴍʟʀᴀɪᴅ ᴄᴏᴜɴᴛ ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ʀᴇᴘʟʏ")
+        await m.reply_text("𝐔sᴀɢᴇ: .𝐃ᴍʟʀᴀɪᴅ 𝐂ᴏᴜɴᴛ 𝐔sᴇʀɴᴀᴍᴇ 𝐎ʀ 𝐑ᴇᴘʟʏ")
         return
       if int(user.id) in VERIFIED_USERS:
          await m.reply_text("𝐒ᴏʀʀʏ 𝐀ᴘ 𝐀ᴘɴᴇ 𝐁ᴀᴀᴘ 𝐏ᴀʀ 𝐑ᴀɪᴅ 𝐍ʜɪ 𝐊ᴀʀ 𝐒ᴀᴋᴛᴇ")
          return
       if int(user.id) in SUDO_USER:
-         await m.reply_text("ᴛʜɪs ɢᴜʏ ɪs ᴀ sᴜᴅᴏ ᴜsᴇʀs.")
+         await m.reply_text("𝐓ʜɪs 𝐆ᴜʏ 𝐈s 𝐀 𝐒ᴜᴅᴏ 𝐔sᴇʀs.")
          return
       mention = user.mention
-      await m.reply_text("ᴅᴍ ʟᴏᴠᴇ ʀᴀɪᴅ sᴛᴀʀᴛᴇᴅ..")
+      await m.reply_text("𝐃ᴍ 𝐋ᴏᴠᴇ 𝐑ᴀɪᴅ 𝐒ᴛᴀʀᴛᴇᴅ..")
       for _ in range(counts): 
          r = f"{choice(LOVE)}"
          await app.send_message(user.id, r)
