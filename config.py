0cb6c8c1ab106d97d8c50237bc2e0d2a957a209a
Aryan Sharma <158464705+Team-CF-Bots@users.noreply.github.com>
2024-02-27 03:22:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "13335263"))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "12380656"))
+API_HASH = getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = int(getenv("OWNER_ID", ""))
 STRING_SESSION = getenv("STRING_SESSION", "")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5392070730").split()))
-ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/a62b9c7d9848afde0569e.jpg")
-REPO_URL = getenv("REPO_URL", "https://github.com/RRomeo-RJ/Romeo-UserBot")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6812888267").split()))
+ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/9db83cb6ae0b870c34e49.jpg")
+REPO_URL = getenv("REPO_URL", "https://github.com/Team-CF-Bots/Lily-Userbot")
 BRANCH = getenv("BRANCH", "main")
