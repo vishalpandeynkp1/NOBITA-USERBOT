b57b9707aa1ab4d6b471af4155fc0d18fd3d1782
vishalpandeynkp1 <nishathakur.nkp@gmail.com>
2024-09-22 10:22:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "12380656"))
-API_HASH = getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
+API_ID = int(getenv("API_ID", "21237427"))
+API_HASH = getenv("API_HASH", "9fddf284152a3792475388ce0bb61435)
+BOT_TOKEN = getenv("BOT_TOKEN", "7895092224:AAHAAtpFYH02h-MXAQDnYzA2oMVLLA6R8V8")
+OWNER_ID = int(getenv("OWNER_ID", "5008302137"))
 STRING_SESSION = getenv("STRING_SESSION", "")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6812888267").split()))
 ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/9db83cb6ae0b870c34e49.jpg")
